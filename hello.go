ee7518c8b8d263dcc748577b27870ec3162e0af4
Sammy0s <Samantha.Yengst@gmail.com>
2026-04-13 16:06:39 -0500

    Run a web page

--- hello.go
+++ hello.go
@@ -1,7 +1,25 @@
 package main
 
-import "fmt"
+import (
+	"fmt"
+	"html/template"
+	"net/http"
+)
+
+func homeHandler(w http.ResponseWriter, r *http.Request) {
+	tmpl, _ := template.ParseFiles("templates/home.html")
+	tmpl.Execute(w, nil)
+}
 
 func main() {
-	fmt.Println("Hello, World")
+	fmt.Println("type localhost:8080/home into your browser")
+
+	http.HandleFunc("/home", homeHandler)
+
+	http.HandleFunc("/about", func(w http.ResponseWriter, r *http.Request) {
+		fmt.Fprintf(w, "About page!")
+	})
+
+	// This MUST be last — it starts the server and blocks everything after it
+	http.ListenAndServe(":8080", nil)
 }
